1e1d1aaddab14c46be4a8166f7db5007fc8411ad
Jesús Urrutia <jesus.urrutia@gmail.com>
2020-06-27 15:36:22 -0400

    feat: setting logger module

---
+++ src/logger.js
@@ -0,0 +1,13 @@
+import { createLogger, format, transports } from 'winston';
+
+import { NODE_ENV, DEBUG_LEVEL } from './config';
+
+const logger = createLogger({
+  level: DEBUG_LEVEL || (NODE_ENV === 'production' ? 'warning' : 'debug'),
+  format: format.json(),
+  transports: [
+    new transports.Console(),
+  ],
+});
+
+export default logger;
